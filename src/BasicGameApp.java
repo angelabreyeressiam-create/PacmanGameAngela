b87c3d21515a3038b6962e575e26cda80cc4a608
angelabreyeressiam <angela_breyer-essiam28@milton.edu>
2026-02-10 08:59:12 -0500

    Initial commit

---
+++ src/BasicGameApp.java
@@ -0,0 +1,225 @@
+//Basic Game Application
+// Basic Object, Image, Movement
+// Threaded
+
+//*******************************************************************************
+//Import Section
+//Add Java libraries needed for the game
+//import java.awt.Canvas;
+
+//Graphics Libraries
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+//*******************************************************************************
+
+public class BasicGameApp implements Runnable {
+
+    //Variable Definition Section
+    //Declare the variables used in the program
+    //You can set their initial values too
+
+    //Sets the width and height of the program window
+    final int WIDTH = 1000;
+    final int HEIGHT = 700;
+
+    //Declare the variables needed for the graphics
+    public JFrame frame;
+    public Canvas canvas;
+    public JPanel panel;
+
+    public BufferStrategy bufferStrategy;
+
+    Pacman pacman;
+    Image packmanImage;
+    BlueGhost blueGhost;
+    Image blueghostImage;
+    Image pinkghostImage;
+    PinkGhost pinkGhost;
+    Image death = Toolkit.getDefaultToolkit().getImage("deathemoji.png");
+    Image backround = Toolkit.getDefaultToolkit().getImage("backround.jpeg");
+
+
+    public boolean firstCrash;
+    public boolean firstcharactercrash;
+    public boolean secondCrash;
+
+    // Main method definition
+    // This is the code that runs first and automatically
+    public static void main(String[] args) {
+        BasicGameApp ex = new BasicGameApp();   //creates a new instance of the game
+        new Thread(ex).start();                 //creates a threads & starts up the code in the run( ) method
+    }
+
+
+    // This section is the setup portion of the program
+    // Initialize your variables and construct your program objects here.
+    public BasicGameApp() { // BasicGameApp constructor
+
+        setUpGraphics();
+        firstcharactercrash = true;
+        secondCrash = true;
+        pacman = new Pacman("pacman.png", 100, 400, 0.75);
+        packmanImage = Toolkit.getDefaultToolkit().getImage("pacman.png");
+        pinkGhost = new PinkGhost("pinkghost.png", 300, 300, 0.75);
+        pinkghostImage = Toolkit.getDefaultToolkit().getImage("pinkghost.png");
+        blueGhost = new BlueGhost("blueghost.png", 500, 200, 0.25);
+        blueghostImage = Toolkit.getDefaultToolkit().getImage("blueghost.png");
+//        voldemort2 = new Voldemort ("voldemort.png", 500, 100, 0.25);
+//        voldemortImage2 = Toolkit.getDefaultToolkit().getImage("voldemort.png");
+        run();
+
+
+    } // end BasicGameApp constructor
+
+
+//*******************************************************************************
+//User Method Section
+// put your code to do things here.
+
+    // main thread
+    // this is the code that plays the game after you set things up
+    public void run() {
+        //for the moment we will loop things forever.
+        while (true) {
+            moveThings();//move all the game objects
+
+            checkCrash();
+            firstEvilCrash();
+            pacmanpinkcrash();
+
+
+            render();  // paint the graphics
+            pause(30); // sleep for 10 ms
+        }
+    }
+
+
+    public void moveThings() {
+        pacman.wrap();
+        blueGhost.bounce();
+        pinkGhost.bounce();
+    }
+// expands harry potter when they crash
+    public void checkCrash(){
+        if(pinkGhost.rect.intersects(blueGhost.rect) && firstCrash == true) {
+            firstCrash = false;
+            blueGhost.dx = -blueGhost.dx;
+            blueGhost.dy = -blueGhost.dy;
+            pinkGhost.dx = -pinkGhost.dx;
+            pinkGhost.dy = -pinkGhost.dy;
+//            voldemort.height += 10;
+//            voldemort.width += 10;
+        }
+        if (pinkGhost.rect.intersects(blueGhost.rect) == false) {
+            firstCrash = true;
+        }
+
+//         if (pacman.isAlive= false){
+//            pacman.isAlive = true;
+//        }
+
+    }
+
+     public void pacmanpinkcrash() {
+            if(pacman.rect.intersects(pinkGhost.rect) && firstcharactercrash == true) {
+                firstcharactercrash = false;
+                pinkGhost.dx = -pinkGhost.dx;
+                pinkGhost.dy = -pinkGhost.dy;
+                pacman.dx = -pacman.dx;
+                pacman.dy = -pacman.dy;
+                pacman.isAlive = false;
+//            voldemort.height += 10;
+//            voldemort.width += 10;
+                }
+            if (pacman.rect.intersects(pinkGhost.rect) == false) {
+                firstcharactercrash = true;
+
+           }
+
+
+          //  for(int x = 0; x < 1000; x = x +10){
+         //       astro1.width += x;
+          //      astro1.height += x;
+
+          //  }
+        }
+    private void firstEvilCrash() {
+        if (pacman.rect.intersects(blueGhost.rect) && secondCrash == true) {
+            secondCrash = false;
+            blueGhost.dx = -blueGhost.dx;
+            blueGhost.dy = -blueGhost.dy;
+            pinkGhost.dx = -pinkGhost.dx;
+            pinkGhost.dy = -pinkGhost.dy;
+//            voldemort.height += 10;
+//            voldemort.width += 10;
+        }
+        if (pinkGhost.rect.intersects(blueGhost.rect) == false) {
+            secondCrash = true;
+        }
+
+    }
+
+
+    //Paints things on the screen using bufferStrategy
+    private void render() {
+        Graphics2D g = (Graphics2D) bufferStrategy.getDrawGraphics();
+        g.clearRect(0, 0, WIDTH, HEIGHT);
+        g.drawImage(backround, 0,0, WIDTH,HEIGHT,null);
+
+        g.setColor(new Color(50,90,80));
+      //  g.fillRect(800,70,(100-voldemort.health),15, voldemort.health);
+        //draw the image
+        if (pacman.isAlive == false) {
+            g.drawImage(death, pacman.xpos, pacman.ypos, pacman.width, pacman.height, null);
+        }  else {
+                g.drawImage(packmanImage, pacman.xpos, pacman.ypos, pacman.width, pacman.height, null);
+            }
+        g.drawImage(blueghostImage, blueGhost.xpos, blueGhost.ypos, blueGhost.width, blueGhost.height, null);
+        g.drawImage(pinkghostImage, pinkGhost.xpos, pinkGhost.ypos, pinkGhost.width, pinkGhost.height, null);
+
+        g.dispose();
+        bufferStrategy.show();
+    }
+
+    //Pauses or sleeps the computer for the amount specified in milliseconds
+    public void pause(int time ) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    //Graphics setup method
+    private void setUpGraphics() {
+        frame = new JFrame("Application Template");   //Create the program window or frame.  Names it.
+
+        panel = (JPanel) frame.getContentPane();  //sets up a JPanel which is what goes in the frame
+        panel.setPreferredSize(new Dimension(WIDTH, HEIGHT));  //sizes the JPanel
+        panel.setLayout(null);   //set the layout
+
+        // creates a canvas which is a blank rectangular area of the screen onto which the application can draw
+        // and trap input events (Mouse and Keyboard events)
+        canvas = new Canvas();
+        canvas.setBounds(0, 0, WIDTH, HEIGHT);
+        canvas.setIgnoreRepaint(true);
+
+        panel.add(canvas);  // adds the canvas to the panel.
+
+        // frame operations
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  //makes the frame close and exit nicely
+        frame.pack();  //adjusts the frame and its contents so the sizes are at their default or larger
+        frame.setResizable(false);   //makes it so the frame cannot be resized
+        frame.setVisible(true);      //IMPORTANT!!!  if the frame is not set to visible it will not appear on the screen!
+
+        // sets up things so the screen displays images nicely.
+        canvas.createBufferStrategy(2);
+        bufferStrategy = canvas.getBufferStrategy();
+        canvas.requestFocus();
+        System.out.println("DONE graphic setup");
+    }
+
+}
---
+++ src/Pacman.java
@@ -0,0 +1,90 @@
+import java.awt.*;
+
+/**
+ * Created by chales on 11/6/2017.
+ * Edits by mblair on 10/27/2025
+ */
+public class Pacman {
+
+    //VARIABLE DECLARATION SECTION
+    //Here's where you state which variables you are going to use.
+    public String name;               //name of the hero
+    public int xpos;                  //the x position
+    public int ypos;                  //the y position
+    public int dx;                    //the speed of the hero in the x direction
+    public int dy;                    //the speed of the hero in the y direction
+    public int width;                 //the width of the hero image
+    public int height;                //the height of the hero image
+    public boolean isAlive;  //a boolean to denote if the hero is alive or dead
+    public Rectangle rect;
+    public double successRate;
+    public int health;
+
+
+    //This is a constructor that takes 3 parameters.
+    // This allows us to specify the hero's name and position when we build it.
+    public Pacman(String pName, int pXpos, int pYpos, double psuccess) {
+        name = pName;
+        xpos = pXpos;
+        ypos = pYpos;
+        dx = 5;
+        dy = 5;
+        width = 150;
+        height = 150;
+        isAlive = true;
+        rect = new Rectangle(xpos,ypos,width,height);
+        successRate = psuccess;
+        health = 500;
+ 
+    }
+
+    public void move() { // move
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        rect = new Rectangle(xpos,ypos,width,height);
+    }
+
+    public void bounce() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        if (xpos >= 1000 - width){
+            dx = -dx;
+        }
+        if (ypos >= 700 - height){
+            dy = -dy;
+        }
+        if (xpos < 0){
+            dx = -dx;
+        }
+        if (ypos < 0){
+            dy = -dy;
+        }
+        rect = new Rectangle(xpos,ypos,width,height);
+
+    }
+
+    public void wrap() {
+        xpos = xpos + dx;
+        ypos = ypos + dy;
+        if (xpos > 1000){
+            xpos = 0;
+        }
+        else if (xpos < 0){
+            xpos = 1000;
+        }
+        if (ypos < 0){
+            ypos = 700;
+        }
+        else if (ypos > 700){
+            ypos = 0;
+        }
+        rect = new Rectangle(xpos,ypos,width,height);
+
+    }
+}
+
+
+
+
+
+
